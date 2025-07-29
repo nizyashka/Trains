3d84633fb9b4a2b51fc77bf84ca3ae4aa7822c3b
Alexey Nepryakhin <alnepryakhin@gmail.com>
2025-07-29 14:38:21 +0300

    Initial Commit

---
+++ Trains/TrainsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsApp.swift
+//  Trains
+//
+//  Created by Алексей Непряхин on 29.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct TrainsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
